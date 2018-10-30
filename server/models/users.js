09a920578d38084660e215d50e8f51940f9f47a3
unknown <orellana2109@gmail.com>
2018-10-30 08:35:41 -0500

    Added POST /users route and token method

--- server/models/users.js
+++ server/models/users.js
@@ -1,26 +1,59 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-// this creates the schema
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        minLength: 1,
-        trim: true
-    }
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        } 
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
-// creating the new user
-var user = new User({
-    email: 'orellana2109@gmail.com'
-});
+// this function lets us only return the id and email rather than the entire object
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
 
-// saving the user
-user.save().then((doc) => {
-    console.log('user saved', doc);
-}, (e) => {
-    console.log('Unable to save user', e);
-});
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+// not using an arrow function because this does not bind
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat({access, token});
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 
 module.exports = {User};
